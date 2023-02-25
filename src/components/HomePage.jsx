365ddca43a8a984713ef0258bd7df929f2622b0c
RozeSullatui <roze23vpa40769@gmail.com>
2023-02-25 16:28:24 +0900

    ListComponent first commit.

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { NestedModal } from "./AddModal";
+import ListComponent from './ListComponent';
 
 function HomePage() {
   return (
     <div>
       <div>HomePage</div>
+      <div>
+        <ListComponent/>
+      </div>
       <div>
         <NestedModal />
       </div>
